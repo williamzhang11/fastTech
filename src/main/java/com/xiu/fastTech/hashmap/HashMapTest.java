fd94ceb43ca48f6b7ef387f18032ffb618dfa8d5
williamzhang11 <1107517306@qq.com>
2019-02-20 18:16:12 +0800

    新增HashMap底层原理

---
+++ src/main/java/com/xiu/fastTech/hashmap/HashMapTest.java
@@ -0,0 +1,15 @@
+package com.xiu.fastTech.hashmap;
+
+import java.util.HashMap;
+
+
+public class HashMapTest {
+
+	public static void main(String[] args) {
+		HashMap<String, Object> map = new HashMap<String,Object>();
+		
+		map.put("1", 1);
+		map.put("2", 3);
+		map.size();
+	}
+}
