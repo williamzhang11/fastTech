3d02749b2c242c4aef21df118134dd719ef2d416
williamzhang11 <1107517306@qq.com>
2019-03-01 15:14:36 +0800

    完善ConcurrentHashMap原理

---
+++ src/main/java/com/xiu/fastTech/multithread/ThreadTest.java
@@ -0,0 +1,27 @@
+package com.xiu.fastTech.multithread;
+
+public class ThreadTest {
+
+	public static void main(String[] args) {
+		
+		Runnable runnable =	new Runnable() {
+			
+			public void run() {
+				try {
+					Thread.sleep(10000L);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				};
+				System.out.println(11111);
+			}
+		};
+		
+		Thread thread = new Thread(runnable);
+		
+		thread.setDaemon(false);
+		thread.start();
+		System.out.println("end");
+		
+	}
+}
